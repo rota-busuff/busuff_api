0670d5cec1664af5415c09d56da6f4477b5ce16c
brenobcc <breno.cvcr@gmail.com>
2025-01-29 19:38:32 -0300

    add: pre-deploy

--- app/config.py
+++ app/config.py
@@ -1,6 +1,10 @@
 import os
 
 class Config:
-    SQALCHEMY_TRACK_MODIFICATIONS = False
-    SQLACHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super_secret_key'
+
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super_jwt_secret_key'
